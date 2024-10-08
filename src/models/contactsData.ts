c16ea4b7e33c0744c1ade9449cdc01467e5ef422
FabricioMeneze5 <fabricio-menezes@live.com>
2024-10-08 02:33:32 +0100

    ADD: contactsData.ts

---
+++ src/models/contactsData.ts
@@ -0,0 +1,48 @@
+import Contact from './Contacts'
+
+const contactsData: Contact[] = [
+  {
+    name: 'Fabricio Menezes',
+    phone: 931358823,
+    email: 'Fabricio-Menezes@live.com',
+    id: 1
+  },
+  {
+    name: 'Barbara Menezes',
+    phone: 948454518,
+    email: 'Barbara-Menezes@live.com',
+    id: 2
+  },
+  {
+    name: 'Rita Menezes',
+    phone: 931358823,
+    email: 'Rita-Menezes@live.com',
+    id: 3
+  },
+  {
+    name: 'Lucas Carmos',
+    phone: 931358823,
+    email: 'Lucas-Carmos@live.com',
+    id: 4
+  },
+  {
+    name: 'Debora Menezes',
+    phone: 931358825,
+    email: 'Debora-Menezes@live.com',
+    id: 5
+  },
+  {
+    name: 'João batista',
+    phone: 934358825,
+    email: 'Debora-Menezes@live.com',
+    id: 6
+  },
+  {
+    name: 'Fabricio da Silva',
+    phone: 931358834,
+    email: 'Debora-Menezes@live.com',
+    id: 7
+  }
+]
+
+export default contactsData
--- src/store/reducers/contacts.ts
+++ src/store/reducers/contacts.ts
@@ -1,56 +1,14 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 import Contact from '../../models/Contacts'
+import contactsData from '../../models/contactsData'
 
 type contactState = {
   items: Contact[]
 }
 
 const initialState: contactState = {
-  items: [
-    {
-      name: 'Fabricio Menezes',
-      phone: 931358823,
-      email: 'Fabricio-Menezes@live.com',
-      id: 1
-    },
-    {
-      name: 'Barbara Menezes',
-      phone: 948454518,
-      email: 'Barbara-Menezes@live.com',
-      id: 2
-    },
-    {
-      name: 'Rita Menezes',
-      phone: 931358823,
-      email: 'Rita-Menezes@live.com',
-      id: 3
-    },
-    {
-      name: 'Lucas Carmos',
-      phone: 931358823,
-      email: 'Lucas-Carmos@live.com',
-      id: 4
-    },
-    {
-      name: 'Debora Menezes',
-      phone: 931358825,
-      email: 'Debora-Menezes@live.com',
-      id: 5
-    },
-    {
-      name: 'João batista',
-      phone: 934358825,
-      email: 'Debora-Menezes@live.com',
-      id: 6
-    },
-    {
-      name: 'Fabricio da Silva',
-      phone: 931358834,
-      email: 'Debora-Menezes@live.com',
-      id: 7
-    }
-  ]
+  items: contactsData
 }
 
 const contactSlice = createSlice({
